f9595d62625b333c568a4df2140584bbb1df1a01
Garrett van Wageningen <ghvanw283@gmail.com>
2021-11-11 08:17:53 -0600

    Update lambda.cs

--- src/lambda.cs
+++ src/lambda.cs
@@ -3,10 +3,7 @@
 IEnumerable<B> Select<A, B>(Func<A, B> fn, IEnumerable<A> items) =>
     items.Select(fn);
 
-var result = Select(x => {
-    var y = 100;
-    return x * y;
-}, new List<int>() { 1, 2, 3, 4, 5 });
+var result = Select(x => x * 100, new List<int>() { 1, 2, 3, 4, 5 });
 
 var printable = string.Join(", ", result.ToList());
 
